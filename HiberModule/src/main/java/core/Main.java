dcfef1d105d6eaf91c058b674117db42881307b6
Alekseev Anton <antonalekseevv.git@gmail.com>
2022-04-09 19:28:42 +0700

    add logback, setting log jdbc agent

--- HiberModule/src/main/java/core/Main.java
+++ HiberModule/src/main/java/core/Main.java
@@ -5,6 +5,8 @@ import core.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -14,22 +16,21 @@ import java.util.List;
  */
 public class Main {
 
-/*    public static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    public static final String USER = "postgres";
-    public static final String PASSWORD = "postgres";*/
-
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+
+        log.info("Hello");
         var serviceRegistry = new StandardServiceRegistryBuilder()
                 .configure("hibernate.cfg.xml").build();
         var metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
+
         SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
         createRecord(User.builder().name("name").build(), sessionFactory);
 
         var session = sessionFactory.openSession();
-        List<City> list = session.createQuery("from model.City", City.class).list();
-        System.out.println(list);
+        List<City> list = session.createQuery("from core.model.City", City.class).list();
     }
 
     private static <T> T createRecord(T record, SessionFactory factory) {
